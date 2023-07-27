fec66c54d98747149a3517c6c9eb2b1360349996
DESKTOP-K5CL10M\neamt <neamtuxandrei@gmail.com>
2023-07-27 10:15:11 +0300

    student feature and structure of app

---
+++ StudentExamPracticeBE/StudentExamPracticeBE.Domain/ExamTask.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentExamPracticeBE.Domain
+{
+    public class ExamTask
+    {
+        public Guid Id { get; private set; }
+        public string Title { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public string Status { get; set; } = "Created";
+        private ExamTask()
+        {
+            
+        }
+        public static ExamTask Create(string title,string description, string status)
+        {
+            if (string.IsNullOrEmpty(title))
+                throw new ArgumentException("The name must not be empty", "firstName");
+
+            var task = new ExamTask();
+            task.Title = title;
+            task.Id = Guid.NewGuid();
+            task.Description = description;
+            task.Status = status;
+            return task;
+        }
+
+    }
+}
---
+++ StudentExamPracticeBE/StudentExamPracticeBE.Domain/Student.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentExamPracticeBE.Domain
+{
+    public class Student : Person
+    {
+        public Guid Id { get; private set; }
+        private List<ExamTask> _tasks = new List<ExamTask>();
+        public ICollection<ExamTask> Tasks => _tasks;
+        private Student()
+        {
+
+        }
+        public static Student Create(string firstName, string lastName, string emailAddress)
+        {
+            Student student = new Student();
+            student.SetName(firstName, lastName);
+            student.SetEmailAddress(emailAddress);
+            return student;
+
+        }
+        public void AddTask(string name, string description, string status)
+        {
+            var task = ExamTask.Create(name, description, status);
+            _tasks.Add(task);
+        }
+        public void RemoveTask(Guid itemToRemove)
+        {
+            var taskFound = _tasks.Where(item => item.Id == itemToRemove)
+                .First();
+            _tasks.Remove(taskFound);
+        }
+        
+    }
+
+}
