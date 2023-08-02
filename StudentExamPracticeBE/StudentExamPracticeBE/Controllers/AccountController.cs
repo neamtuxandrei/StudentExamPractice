3a25fdc9589d6935b648781a99bd379f4e8cad1c
Vlad <just_vlad@yahoo.com>
2023-08-02 23:24:49 +0300

    Add controller for user roles

---
+++ StudentExamPracticeBE/StudentExamPracticeBE/Controllers/AccountController.cs
@@ -0,0 +1,35 @@
+﻿//using Microsoft.AspNet.Identity;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using StudentExamPracticeBE.Models;
+
+namespace StudentExamPracticeBE.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public AccountController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetUserRoles(string email)
+        {
+            var user = await _userManager.FindByEmailAsync(email);
+            if (user != null)
+            {
+                var roles = await _userManager.GetRolesAsync(user);
+
+                if (roles != null)
+                    return Ok(roles);
+            }
+
+            return NotFound("User or role not found.");
+        }
+    }
+}
